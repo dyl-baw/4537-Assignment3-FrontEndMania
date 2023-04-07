86131a34b6aff61f2850ef2d0187d8d267a79a75
dyl-baw <dvd.y.lee@gmail.com>
2023-04-06 20:45:09 -0700

    completed Axios call

--- my-app/src/Search.js
+++ my-app/src/Search.js
@@ -1,6 +1,20 @@
 import React from 'react'
+import axios from 'axios'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
 function Search() {
+    const [Types, setTypes] = useState([])
+
+
+   useEffect(() => {
+    async function fetchTypes() {
+      const response = await axios.get('https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/types.json')
+      setTypes(response.data.map(type => type.english))
+    }
+    fetchTypes()
+  }, [])
+
   return (
     <div>Search</div>
   )
