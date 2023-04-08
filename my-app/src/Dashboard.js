2d56dec24a02445b57182ed86b12e88f16e3ad79
dyl-baw <dvd.y.lee@gmail.com>
2023-04-07 23:40:47 -0700

    added: filtering of types

--- my-app/src/Dashboard.js
+++ my-app/src/Dashboard.js
@@ -1,6 +1,8 @@
+import Search from "./Search";
+import Result from "./Result";
 import React from 'react'
 import Report from './Report';
-
+import { useState } from "react";
 
 import {
   Routes,
@@ -9,6 +11,9 @@ import {
 } from "react-router-dom";
 
 function Dashboard({ accessToken, setAccessToken, refreshToken }) {
+
+  const [selectedTypes, setSelectedTypes] = useState([])
+
   return (
     <div>
       <h1>
@@ -16,7 +21,7 @@ function Dashboard({ accessToken, setAccessToken, refreshToken }) {
       </h1>
       <nav>
         <ul>
-          <li><Link to="/report/1">Report 1 - Unique API users over a period of time</Link></li>
+          <li><Link to="/report/1">Report 1  - Unique API users over a period of time</Link></li>
           <li><Link to="/report/2">Report 2 - Top API users over period of time</Link></li>
           <li><Link to="/report/3">Report 3 - Top users for each Endpoint</Link></li>
         </ul>
@@ -28,6 +33,13 @@ function Dashboard({ accessToken, setAccessToken, refreshToken }) {
         <Route path="/report/3" element={<Report id={3} accessToken={accessToken} setAccessToken={setAccessToken} refreshToken={refreshToken} />} />
       </Routes>
 
+      <Search 
+        selectedTypes={selectedTypes}
+        setSelectedTypes={setSelectedTypes}
+      />
+      <Result
+      selectedTypes={selectedTypes}
+      /> 
     </div>
   )
 }
