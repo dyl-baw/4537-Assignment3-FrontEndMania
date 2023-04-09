85e00c7c9cdba0f680ed4c1fc53fb771ee967b5c
dyl-baw <dvd.y.lee@gmail.com>
2023-04-09 01:44:47 -0700

    adjusted reports

--- my-app/src/Dashboard.js
+++ my-app/src/Dashboard.js
@@ -1,8 +1,7 @@
 import Search from "./Search";
-import Result from "./Result";
+// import Result from "./Result";
 import React from 'react'
 import Report from './Report';
-import { useState } from "react";
 
 import {
   Routes,
@@ -10,38 +9,24 @@ import {
   Link
 } from "react-router-dom";
 
-function Dashboard({ accessToken, setAccessToken, refreshToken }) {
-
-  const [selectedTypes, setSelectedTypes] = useState([])
-
+function Dashboard({ user, setUser, accessToken, setAccessToken, refreshToken }) {
   return (
-    <div>
-      <h1>
-        Dashboard
-      </h1>
-      <nav>
-        <ul>
-          <li><Link to="/report/1">Report 1  - Unique API users over a period of time</Link></li>
-          <li><Link to="/report/2">Report 2 - Top API users over period of time</Link></li>
-          <li><Link to="/report/3">Report 3 - Top users for each Endpoint</Link></li>
-        </ul>
-      </nav>
-
-      <Routes>
-        <Route path="/report/1" element={<Report id={1} accessToken={accessToken} setAccessToken={setAccessToken} refreshToken={refreshToken} />} />
-        <Route path="/report/2" element={<Report id={2} accessToken={accessToken} setAccessToken={setAccessToken} refreshToken={refreshToken} />} />
-        <Route path="/report/3" element={<Report id={3} accessToken={accessToken} setAccessToken={setAccessToken} refreshToken={refreshToken} />} />
-      </Routes>
-
-      <Search 
-        selectedTypes={selectedTypes}
-        setSelectedTypes={setSelectedTypes}
-      />
-      <Result
-      selectedTypes={selectedTypes}
-      /> 
-    </div>
+    <>{
+        user?.role === 'admin' ? (
+            <div>
+                <h1>
+                    Dashboard
+                </h1>
+                <nav>
+                    <ul>
+                        <li><Link to="/report/1">Report 1 - Unique API users over a period of time</Link></li>
+                        <li><Link to="/report/2">Report 2 - Top API users over period of time</Link></li>
+                        <li><Link to="/report/3">Report 3 - Top users for each Endpoint</Link></li>
+                    </ul>
+                </nav>
+            </div>)
+            : null}
+    </>
   )
 }
-
 export default Dashboard
