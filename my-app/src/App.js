3d4ae8786cbb147fc59398f0a69cafea0c874f3e
dyl-baw <dvd.y.lee@gmail.com>
2023-04-09 18:11:48 -0700

    attempting to fix deployment errors

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,8 @@
 // import Result from "./Result";
 import { useState } from "react";
 import Login from "./Login";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 
 function App() {
   return (
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style.css';
 import App from './App';
-<script src="https://unpkg.com/react-router-dom/umd/react-router-dom.min.js"></script>
 // import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 
